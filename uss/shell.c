e02c8d31da4a980c63f1528c124d3f7b48f0b40c
ajmcb <contact@ajmcb.com>
2016-01-29 22:24:54 +0000

    nearly done

--- uss/shell.c
+++ uss/shell.c
@@ -1,19 +1,110 @@
 #include <string.h>
 #include <stdio.h>
 #include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 void signal_handler(int);
 void shell_prompt();
-void read_input();
+void read_input(char*);
 
 int main() {
-    signal(SIGINT, signal_handler);
+  char *command = NULL;
+  int read = 0;
+  char *commandArray[read];
+  char *cmd;
+  char *savePtr;
+  char first[sizeof(cmd)+1];
+  char ex[5];
+  char cd[3];
+  char pwd[4];
+  char cwd[1024];
+  char *wd;
+  size_t len = 0;
+  int i = 0;
+  int success = -1;
+  int exitCode = 0;
+  pid_t child;
+  pid_t w;
+  int *childStatus;
 
-    while(1) {
-        shell_prompt();
-        read_input();
-    }
-    return 0;
+  signal(SIGINT, signal_handler);
+
+  strcpy(ex, "exit");
+  strcpy(cd, "cd");
+  strcpy(pwd, "pwd");
+
+  while(1) {
+      shell_prompt();
+
+      read = getline(&command, &len, stdin);
+
+      if (read == -1) {
+        printf("Failed to read user input. Exiting...\n");
+        exitCode = EXIT_FAILURE;
+        break;
+      } else if (read > 1) {
+        command[read - 1] = 0;
+      } else {
+        continue;
+      }
+
+      //first = strtok(command, " ");
+      cmd = strtok_r(command, " ", &savePtr);
+      strcpy(first, cmd);
+      printf("First: %s\n", first);
+      printf("Rest: ");
+
+      while (cmd != NULL) {
+        commandArray[i++] = cmd;
+        printf(cmd);
+        cmd = strtok_r(NULL, " ", &savePtr); //_r
+      }
+
+      if (strcmp(first, ex) == 0) {
+            printf("Exiting...\n");
+            break;
+      } else if (strcmp(first, cd) == 0) {
+            success = chdir(commandArray[1]);
+            if (success != 0) {
+              printf("No such file or directory.\n");
+            }
+      } else if (strcmp(first, pwd) == 0) {
+            wd = getcwd(cwd, sizeof(cwd));
+            if (wd != NULL) {
+              printf("%s\n", cwd);
+            }
+      } else {
+            child = fork();
+            if (child == -1) {
+              printf("Could not fork and exec. Exiting...\n");
+              exitCode = EXIT_FAILURE;
+              break;
+            } else if (child == 0) {
+              execvp(first, commandArray);
+              exitCode = EXIT_FAILURE;
+              break;
+            } else {
+              w = wait(childStatus);
+              if (w == -1) {
+                printf("Error in child process. Exiting...");
+                exitCode = EXIT_FAILURE;
+                break;
+              }
+
+              if (WIFEXITED(childStatus)) {
+                printf("Child process terminated. Exiting...");
+                exitCode = WEXITSTATUS(childStatus);
+                break;
+              }
+            }
+      }
+
+  }
+
+  free(command);
+  exit(exitCode);
+  return 0;
 }
 
 void signal_handler(int sig) {
@@ -22,11 +113,16 @@ void signal_handler(int sig) {
 }
 
 void shell_prompt() {
-    printf("Shell >>");
+    printf("Shell >> ");
 }
 
-void read_input() {
-    char command[100];
+void read_input(char *command) {
+    size_t len = 0;
+    ssize_t read;
 
-    fgets(command, 100, stdin);
+    read = getline(&command, &len, stdin);
+
+    if (read == -1) {
+      printf("ERROR");
+    }
 }
